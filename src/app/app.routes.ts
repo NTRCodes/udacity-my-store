9105c4ff8d65e820eee3a48a40113f33c277a4c2
NTRCodes <nr44.coder@gmail.com>
2024-09-04 15:42:47 -0700

    implemented navigation
    added routerLink and routerOutlet to the header component

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
+import { ProductListComponent } from './product-list/product-list.component';
+import { CartComponent } from './cart/cart.component';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    title: 'Product List',
+    component: ProductListComponent,
+  },
+  {
+    path: 'detail/:id',
+    title: 'Product Detail',
+    component: ProductDetailComponent,
+  },
+  {
+    path: 'cart',
+    title: 'Cart',
+    component: CartComponent,
+  }
+];
